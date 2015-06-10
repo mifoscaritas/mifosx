f2ba8fb58d779b6211cd4c26ba77ad2c1e023649
awaleram <ram.awale@confuxtechnologies.com>
2015-06-10 16:43:19 +0530

    caritas demo with nely added api constants

---
+++ mifosng-provider/src/main/java/org/mifosplatform/portfolio/saving_investment/api/SavingInvestmentConstants.java
@@ -0,0 +1,17 @@
+package org.mifosplatform.portfolio.saving_investment.api;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class SavingInvestmentConstants {
+    
+    public static final String savingIdParamName = "savingId";
+    public static final String loanIdParamName = "loanId";
+    
+    
+    public static final Set<String> SAVINGINVESTMENT_CREATE_REQUEST_DATA_PARAMETERS = new HashSet<>(Arrays.asList(savingIdParamName,
+            loanIdParamName));
+
+}
